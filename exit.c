57919fa9c21431e2b4a207aad5cbca954f998626
mwape365 <unratedideas@gmail.com>
2022-06-17 23:49:14 -0700

    exit status

---
+++ exit.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+#include <stdlib.h>
+
+/**
+ * _exit - exit the interpreter
+ *
+ * @status: integer value to exit with
+ *
+ * Description: free malloced memory and exit with status
+ */
+void _exit(int status)
+{
+	free_dlistint(global.opstack_tail);
+	if (global.opcode)
+		free(global.opcode);
+	fclose(global.fp);
+	exit(status);
+}
